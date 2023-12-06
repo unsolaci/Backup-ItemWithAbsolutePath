d69a02844ff82b7784786d78a3297f10704edc4e
Wojciech Ros <code@unsola.ci>
2023-12-06 20:00:36 +0100

    docs: add PSScriptInfo

--- Backup-ItemWithAbsolutePath.psm1
+++ Backup-ItemWithAbsolutePath.psm1
@@ -1,3 +1,41 @@
+<#PSScriptInfo
+.VERSION     1.0.0
+.DESCRIPTION Copies files and directories including absolute paths.
+.AUTHOR      Wojciech Ros (code@unsola.ci)
+.COPYRIGHT   Copyright 2023 Wojciech Ros (code@unsola.ci)
+.LICENSEURI  http://www.apache.org/licenses/LICENSE-2.0
+.GUID        ccc7658f-3ac5-4bb1-ac21-6c8032c44056
+#>
+
+<#
+.SYNOPSIS
+    Copies files and directories including absolute paths.
+.DESCRIPTION
+    Inspired by 7-Zip's `-spf` switch, copies files and directories,
+    including the full specified absolute source paths into the
+    destination path, creating all the necessary parent directories
+    if they don't exist.
+
+    For example:
+
+    > Backup-ItemWithAbsolutePath `
+        -Path "/etc/sourcedirectory/source.file" `
+        -Destinaion "/home/user/backup/"
+
+    will copy `source.file` into `/home/user/backup/etc/sourcedirectory/`.
+
+    **Note:** The path created in the destination directory will be exactly
+              as specified in the source path, i.e. if the specified source
+              path is not fully qualified, it will NOT be converted to a
+              fully qualified path, e.g.:
+
+    > Backup-ItemWithAbsolutePath `
+        -Path "./sourcedirectory/source.file" `
+        -Destinaion "/home/user/backup/"
+
+    will copy `source.file` into `/home/user/backup/sourcedirectory/`.
+#>
+
 function Backup-ItemWithAbsolutePath {
     [CmdletBinding(SupportsShouldProcess=$true)]
     Param (
